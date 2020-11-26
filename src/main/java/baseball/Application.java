f776d2055760b1383a6d9e6e36de6fda2585b082
jun <tjrwns1260@naver.com>
2020-11-26 17:03:51 +0900

    게임 자체의 구현 완료. 아직 미비한 사항 1) 자바 코드 컨벤션 지키지 않음 2) 메소드 더 섬세하게 분리할 수 있게 파악 필요 3) ball을 계산하는 방법 개선 필요 4) 비정상적인 입력에 대해 고려하지 않음(IllegalArgumentExeption 구현 필요) 5) 각 클래스의 접근제어자에 대해 더 생각해보기 6) scanner 필요할때 해당 클래스에서 다시 선언해야 하는게 맞는건지? 왜 오류가 뜨는지 확인( 왜 선언 안하면 scanner cannot be resolved 에러 뜨는지) 7)파이썬으로만 하다가 자바로 하려니 각 인덱스 활용이 어렵다. String을 이렇게 활용하는게 맞는건지.. 각 숫자를 string으로 저장해서 비교하는데 보통 자바에서는 어떻게 해야하는지 확인이 필요하다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,20 +9,17 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         boolean gameCoin = true;
-        int a = 0;
         while(gameCoin){
-            int ball = 0;
-            int strike = 0;
             String computerChoice = computerChoose();
-            System.out.print(computerChoice);
+            gameStart(computerChoice);
             gameCoin=checkGameCoin();
         }
     }
 
     private static boolean checkGameCoin(){
-        Scanner sc = new Scanner(System.in);  //왜 여기 이렇게 해줘야 오류(scanner cannot be resolved)없이 실행?
+        Scanner sc = new Scanner(System.in);
         int gameCoin=0;
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         gameCoin = sc.nextInt();
         return gameCoin==1;
     }
@@ -38,8 +35,67 @@ public class Application {
             answer[2] = RandomUtils.nextInt(1,9);
         }while(answer[0]==answer[2] || answer[1]==answer[2]);
         
-        computerChoice = Arrays.toString(answer);
+        computerChoice = Arrays.toString(answer).replaceAll("[^0-9]","");
         return computerChoice;
     }
-}
 
+    private static void gameStart(String computerChoice){
+        int strike;
+        String userChoice = userChoose();
+        strike = umpireThePitch(computerChoice, userChoice);
+        if(strike==3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return;
+        }else{
+            gameStart(computerChoice);
+        }
+    }
+
+    private static String userChoose(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        String userChoice = sc.nextLine();
+            return userChoice;
+    }
+
+    private static int umpireThePitch(String computerChoice, String userChoice){
+        int strike = 0;
+        int ball = 0;
+        
+        for(int i=0; i< computerChoice.length(); i++){
+            if(computerChoice.charAt(i)==userChoice.charAt(i)){
+                strike+=1;
+            }
+        }
+        for(int i=0; i< computerChoice.length(); i++){
+            if(userChoice.charAt(0)==computerChoice.charAt(i)){
+                ball+=1;
+            }
+        }
+        for(int i=0; i< computerChoice.length(); i++){
+            if(userChoice.charAt(1)==computerChoice.charAt(i)){
+                ball+=1;
+            }
+        }
+        for(int i=0; i< computerChoice.length(); i++){
+            if(userChoice.charAt(2)==computerChoice.charAt(i)){
+                ball+=1;
+            }
+        }
+        ball-=strike;
+        umpireCalls(strike,ball);
+        return strike;
+    }
+
+    private static void umpireCalls(int strike, int ball){
+        if(strike == 0 && ball == 0){
+            System.out.println("낫싱");
+        }else if(strike == 0 && ball != 0){
+            System.out.println(ball+"볼");
+        }else if(strike !=0 && ball == 0){
+            System.out.println(strike+"스트라이크");
+        }else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+    }
+}
